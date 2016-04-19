7c704c09be75d389a98be497ec53b87c33378d3f
John Calabrese <jcalabrese@pivotal.io>
2016-04-18 23:58:09 -0400

    continuing buildout of concourse deployment

--- deployment/concourse/concourse.go
+++ deployment/concourse/concourse.go
@@ -1,10 +1,14 @@
 package concourse
 
-import "github.com/xchapter7x/enaml"
+import (
+	"github.com/xchapter7x/enaml"
+	"github.com/xchapter7x/enaml-concourse-sample/releasejobs"
+)
 
 var (
-	DefaultName  = "concourse"
-	DirectorUUID = "asdfasdfasdf"
+	DefaultName   = "concourse"
+	DirectorUUID  = "asdfasdfasdf"
+	StemcellAlias = "trusty"
 )
 
 type Deployment struct {
@@ -19,10 +23,27 @@ func NewDeployment() (d Deployment) {
 	d.Manifest.SetDirectorUUID(DirectorUUID)
 	d.Manifest.AddReleaseByName("concourse")
 	d.Manifest.AddReleaseByName("garden-linux")
-	d.Manifest.AddStemcellByName("ubuntu-trusty", "trusty")
+	d.Manifest.AddStemcellByName("ubuntu-trusty", StemcellAlias)
+	web := enaml.NewInstanceGroup("web", 1, "web", StemcellAlias)
+	web.AddAZ("z1")
+	web.AddNetwork(enaml.InstanceGroupNetwork{"name": "private"})
+	atc := enaml.NewInstanceJob("atc", "concourse", releasejobs.Atc{
+		ExternalUrl:        "something",
+		BasicAuthUsername:  "user",
+		BasicAuthPassword:  "password",
+		PostgresqlDatabase: "&atc_db atc",
+	})
+	tsa := enaml.NewInstanceJob("tsa", "concourse", releasejobs.Tsa{})
+	web.AddJob(atc)
+	web.AddJob(tsa)
+	db := enaml.NewInstanceGroup("db", 1, "database", StemcellAlias)
+	worker := enaml.NewInstanceGroup("worker", 1, "worker", StemcellAlias)
+	d.Manifest.AddInstanceGroup(web)
+	d.Manifest.AddInstanceGroup(db)
+	d.Manifest.AddInstanceGroup(worker)
 	return
 }
 
-func (s Deployment) VSphere() enaml.DeploymentManifest {
+func (s Deployment) GetDeployment() enaml.DeploymentManifest {
 	return *s.Manifest
 }
