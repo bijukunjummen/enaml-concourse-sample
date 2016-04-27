5e9b58fad3ca522db653766bdd448151273e4a08
John Calabrese <jcalabrese@pivotal.io>
2016-04-27 17:52:47 -0400

    [#118197197] we have a working test to validate HA in our AZs

--- deployment/cloud-config/aws_test.go
+++ deployment/cloud-config/aws_test.go
@@ -1,39 +1,38 @@
 package cloudconfig_test
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/xchapter7x/enaml"
 	. "github.com/xchapter7x/enaml-concourse-sample/deployment/cloud-config"
 )
 
-var _ = XDescribe("given CloudConfig Deployment for AWS", func() {
-	var awsConfig enaml.DeploymentManifest
+var _ = Describe("given CloudConfig Deployment for AWS", func() {
+	var awsConfig enaml.CloudConfigManifest
 	BeforeEach(func() {
 		awsConfig = NewAWSCloudConfig()
-		fmt.Println(awsConfig)
 	})
 
 	Context("when AZs are defined", func() {
 		It("then we should always have a HA configuration", func() {
-			Ω(1).Should(Equal(1))
+			haCount := 2
+			numberOfAZs := len(awsConfig.AZs)
+			Ω(numberOfAZs).Should(BeNumerically(">=", haCount))
 		})
 	})
-	Context("when a user of the iaas would like to define vm types", func() {
+	XContext("when a user of the iaas would like to define vm types", func() {
 		It("then they should have the option of a small and a large VM configuration", func() {
 			Ω(1).Should(Equal(1))
 		})
 	})
-	Context("when a user of the iaas would like to assign disk", func() {
+	XContext("when a user of the iaas would like to assign disk", func() {
 		It("then they should have the option of small or large capacity configurations", func() {
 
 			Ω(1).Should(Equal(1))
 		})
 	})
 
-	Context("when a user of the iaas would like to assign a network", func() {
+	XContext("when a user of the iaas would like to assign a network", func() {
 		It("then they should have the option of a private and a vip network", func() {
 
 			Ω(1).Should(Equal(1))
