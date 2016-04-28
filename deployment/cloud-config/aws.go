1e182dc759245207c6a321bdff8cbcc80a95c4dc
John Calabrese <jcalabrese@pivotal.io>
2016-04-28 11:16:05 -0400

    [#118197197] adding vmtype declaration to cloud config for aws

--- deployment/cloud-config/aws.go
+++ deployment/cloud-config/aws.go
@@ -1,14 +1,62 @@
 package cloudconfig
 
-import "github.com/xchapter7x/enaml"
+import (
+	"github.com/xchapter7x/enaml"
+	"github.com/xchapter7x/enaml/cloudproperties/aws"
+)
+
+const (
+	SmallVMName            = "small"
+	SmallVMSize            = "t2.micro"
+	LargeVMName            = "large"
+	LargeVMSize            = "m3.medium"
+	LargeDiskType          = "gp2"
+	LargeEphemeralDiskSize = 30000
+	SmallDiskType          = "gp2"
+	SmallEphemeralDiskSize = 3000
+)
 
 func NewAWSCloudConfig() (awsCloudConfig enaml.CloudConfigManifest) {
 	awsCloudConfig = enaml.CloudConfigManifest{}
 	AddAZs(&awsCloudConfig)
+	AddDisk(&awsCloudConfig)
+	AddNetwork(&awsCloudConfig)
+	AddVMTypes(&awsCloudConfig)
 	return
 }
 
 func AddAZs(cfg *enaml.CloudConfigManifest) {
-	cfg.AZs = append(cfg.AZs, enaml.AZ{})
-	cfg.AZs = append(cfg.AZs, enaml.AZ{})
+	cfg.AddAZ(enaml.AZ{})
+	cfg.AddAZ(enaml.AZ{})
+}
+
+func AddDisk(cfg *enaml.CloudConfigManifest) {
+
+}
+
+func AddNetwork(cfg *enaml.CloudConfigManifest) {
+
+}
+
+func AddVMTypes(cfg *enaml.CloudConfigManifest) {
+	cfg.AddVMType(enaml.VMType{
+		Name: SmallVMName,
+		CloudProperties: awscloudproperties.ResourcePool{
+			InstanceType: SmallVMSize,
+			EphemeralDisk: awscloudproperties.EphemeralDisk{
+				Size:     SmallEphemeralDiskSize,
+				DiskType: SmallDiskType,
+			},
+		},
+	})
+	cfg.AddVMType(enaml.VMType{
+		Name: LargeVMName,
+		CloudProperties: awscloudproperties.ResourcePool{
+			InstanceType: LargeVMSize,
+			EphemeralDisk: awscloudproperties.EphemeralDisk{
+				Size:     LargeEphemeralDiskSize,
+				DiskType: LargeDiskType,
+			},
+		},
+	})
 }
--- deployment/cloud-config/aws_test.go
+++ deployment/cloud-config/aws_test.go
@@ -1,10 +1,13 @@
 package cloudconfig_test
 
 import (
+	"errors"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/xchapter7x/enaml"
 	. "github.com/xchapter7x/enaml-concourse-sample/deployment/cloud-config"
+	"github.com/xchapter7x/enaml/cloudproperties/aws"
 )
 
 var _ = Describe("given CloudConfig Deployment for AWS", func() {
@@ -20,11 +23,56 @@ var _ = Describe("given CloudConfig Deployment for AWS", func() {
 			Ω(numberOfAZs).Should(BeNumerically(">=", haCount))
 		})
 	})
-	XContext("when a user of the iaas would like to define vm types", func() {
-		It("then they should have the option of a small and a large VM configuration", func() {
-			Ω(1).Should(Equal(1))
+	Context("when a user of the iaas would like to define vm types", func() {
+		It("then there should be 2 vm type options available", func() {
+			Ω(len(awsConfig.VMTypes)).Should(Equal(2))
+		})
+
+		It("then they should have the option of a small VM configuration", func() {
+			_, err := getVmTypeByName(SmallVMName, awsConfig.VMTypes)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		Context("when the vmtype is small", func() {
+			var vm enaml.VMType
+			BeforeEach(func() {
+				vm, _ = getVmTypeByName(SmallVMName, awsConfig.VMTypes)
+			})
+			It("then it should use a t2.micro size aws instance", func() {
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).InstanceType).Should(Equal(SmallVMSize))
+			})
+
+			It("then it should use a properly configured ephemeral disk", func() {
+				properSmallDiskSize := SmallEphemeralDiskSize
+				properDiskType := SmallDiskType
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).EphemeralDisk.Size).Should(Equal(properSmallDiskSize))
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).EphemeralDisk.DiskType).Should(Equal(properDiskType))
+			})
+		})
+
+		It("then they should have the option of a large VM configuration", func() {
+			_, err := getVmTypeByName(LargeVMName, awsConfig.VMTypes)
+			Ω(err).ShouldNot(HaveOccurred())
+		})
+
+		Context("when the vmtype is large", func() {
+			var vm enaml.VMType
+			BeforeEach(func() {
+				vm, _ = getVmTypeByName(LargeVMName, awsConfig.VMTypes)
+			})
+			It("then it should use a m3.medium size aws instance", func() {
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).InstanceType).Should(Equal(LargeVMSize))
+			})
+
+			It("then it should use a properly configured ephemeral disk", func() {
+				properLargeDiskSize := LargeEphemeralDiskSize
+				properDiskType := LargeDiskType
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).EphemeralDisk.Size).Should(Equal(properLargeDiskSize))
+				Ω(vm.CloudProperties.(awscloudproperties.ResourcePool).EphemeralDisk.DiskType).Should(Equal(properDiskType))
+			})
 		})
 	})
+
 	XContext("when a user of the iaas would like to assign disk", func() {
 		It("then they should have the option of small or large capacity configurations", func() {
 
@@ -47,3 +95,14 @@ var _ = Describe("given CloudConfig Deployment for AWS", func() {
 		})
 	})
 })
+
+func getVmTypeByName(name string, vmTypes []enaml.VMType) (res enaml.VMType, err error) {
+	err = errors.New("no type found")
+	for _, k := range vmTypes {
+		if k.Name == name {
+			err = nil
+			res = k
+		}
+	}
+	return
+}
